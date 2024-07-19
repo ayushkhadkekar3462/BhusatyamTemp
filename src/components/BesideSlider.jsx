a1ee8b88318a3680d8b12cccd48a8dd57ec5133d
ayushkhadkekar3462 <ee220002022@iiti.ac.in>
2024-07-20 02:41:51 +0530

    Made Image Slider

---
+++ src/components/BesideSlider.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../styles/componentstyles/Besidecard.css"
+
+
+export default function BesideSlider() {
+  return (
+    <>
+     <div className="besidecard">
+        <div className="title">Buy land near you</div>
+        <div className="content">Search land and lifestyle real estate including homes with land, vacant land, rural property, commercial lots, and more.</div>
+        <button className="besidesliderbtn">Land for sale near me</button>
+     </div>
+    </>
+  )
+}
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Header from '../../components/header'
 import Searchbar from '../../components/Searchbar'
+import CardSlider from '../../components/CardSlider'
+import BesideSlider from '../../components/BesideSlider'
+import "../../styles/componentstyles/Home.css"
 
 function Home() {
   return (
     <>
     <Header />
     <Searchbar/>
+    <div className='sliderencloser'><div className='wholeslider'>
+      <BesideSlider/>
+    <CardSlider/></div></div>
+    
+    
+   
     
     </>
 
